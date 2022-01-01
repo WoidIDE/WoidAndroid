4c227e7435a96753797b1f2e5ac85ddfb527366f
TheClashFruit <clash.fruit123@gmail.com>
2022-01-01 15:42:22 +0100

    feat: theme stuff, splash screen

--- app/src/main/java/xyz/theclashfruit/woid/MainActivity.java
+++ app/src/main/java/xyz/theclashfruit/woid/MainActivity.java
@@ -2,7 +2,11 @@ package xyz.theclashfruit.woid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#14181f"));
     }
 }
