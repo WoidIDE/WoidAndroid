c6006d649d3fbec5856995000730d688bcff0e0c
TheClashFruit <clash.fruit123@gmail.com>
2022-05-10 18:09:55 +0200

    feat: change some stuff on EditorActivity

--- app/src/main/java/xyz/theclashfruit/woid/EditorActivity.java
+++ app/src/main/java/xyz/theclashfruit/woid/EditorActivity.java
@@ -2,18 +2,14 @@ package xyz.theclashfruit.woid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.material.tabs.TabLayout;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -21,7 +17,7 @@ import java.io.IOException;
 import io.github.rosemoe.sora.widget.CodeEditor;
 import xyz.theclashfruit.woid.fragments.EditorCodeFragment;
 import xyz.theclashfruit.woid.fragments.EditorFilesFragment;
-import xyz.theclashfruit.woid.fragments.EditorLayoutFragment;
+import xyz.theclashfruit.woid.fragments.HomeFragment;
 import xyz.theclashfruit.woid.gson.ProjectMetaGson;
 import xyz.theclashfruit.woid.utils.StorageUtil;
 import xyz.theclashfruit.woid.utils.WoidUtils;
@@ -29,11 +25,8 @@ import xyz.theclashfruit.woid.utils.WoidUtils;
 public class EditorActivity extends AppCompatActivity {
 
   private Toolbar toolBar;
-  private ViewPager viewPager;
-  private TabLayout tabLayout;
 
   private EditorCodeFragment editorCodeFragment;
-  private EditorLayoutFragment editorLayoutFragment;
   private EditorFilesFragment editorFilesFragment;
 
   String currentFile;
@@ -44,8 +37,6 @@ public class EditorActivity extends AppCompatActivity {
     setContentView(R.layout.activity_editor);
 
     toolBar   = findViewById(R.id.toolBar);
-    viewPager = findViewById(R.id.viewPager);
-    tabLayout = findViewById(R.id.tabLayout);
 
     setSupportActionBar(toolBar);
 
@@ -61,40 +52,26 @@ public class EditorActivity extends AppCompatActivity {
     try {
       projectMeta = gson.fromJson(StorageUtil.readFile(intent.getStringExtra("projectPath") + "/meta.json"), ProjectMetaGson.class);
 
-      getSupportActionBar().setSubtitle(projectMeta.getProjectName().replaceAll("\\s+",""));
+      getSupportActionBar().setSubtitle(projectMeta.getProjectName().replaceAll("\\s+","").concat("- MainActivity.java*"));
 
       currentFile = intent.getStringExtra("projectPath") + "/android/app/src/main/java/" + projectMeta.getPackageName().replaceAll("\\.","/") + "/MainActivity.java";
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    viewPager.setAdapter(new FragmentAdapter(getApplicationContext(), getSupportFragmentManager(), 3));
-    tabLayout.setupWithViewPager(viewPager);
 
-    viewPager.setOffscreenPageLimit(4);
+    editorCodeFragment = new EditorCodeFragment();
+    editorFilesFragment = new EditorFilesFragment();
 
-    editorCodeFragment = (EditorCodeFragment) viewPager.getAdapter().instantiateItem(viewPager, 0);
-    editorLayoutFragment = (EditorLayoutFragment) viewPager.getAdapter().instantiateItem(viewPager, 1);
-    editorFilesFragment = (EditorFilesFragment) viewPager.getAdapter().instantiateItem(viewPager, 2);
+    editorCodeFragment.currentFile = currentFile;
 
-    editorCodeFragment.currentFile = intent.getStringExtra("projectPath") + "/android/app/src/main/java/" + projectMeta.getPackageName().replaceAll("\\.","/") + "/MainActivity.java";
+    if(savedInstanceState == null) {
+      FragmentManager fragmentManager = getSupportFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-    viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-      @Override
-      public void onPageScrolled(int _position, float _positionOffset, int _positionOffsetPixels) {
-
-      }
-
-      @Override
-      public void onPageSelected(int _position) {
-
-      }
-
-      @Override
-      public void onPageScrollStateChanged(int _scrollState) {
-
-      }
-    });
+      fragmentTransaction.replace(R.id.fragmentContainer, editorCodeFragment);
+      fragmentTransaction.commit();
+    }
   }
 
   @Override
@@ -104,6 +81,9 @@ public class EditorActivity extends AppCompatActivity {
             .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     menu.add("Save")
             .setIcon(R.drawable.ic_save)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    menu.add("Files")
+            .setIcon(R.drawable.ic_folder)
             .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     menu.add("Import Files")
             .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
@@ -132,39 +112,25 @@ public class EditorActivity extends AppCompatActivity {
       WoidUtils.showToast(getApplicationContext(), "File \"" + last + "\" Saved.");
 
       return true;
+    } else if ("Files".contentEquals(title)) {
+      FragmentManager fragmentManager = getSupportFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+      fragmentTransaction.replace(R.id.fragmentContainer, editorFilesFragment);
+      fragmentTransaction.commit();
+
+      item.setTitle("Editor");
+      item.setIcon(R.drawable.ic_edit);
+    } else if ("Editor".contentEquals(title)) {
+      FragmentManager fragmentManager = getSupportFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+      fragmentTransaction.replace(R.id.fragmentContainer, editorCodeFragment);
+      fragmentTransaction.commit();
+
+      item.setTitle("Files");
+      item.setIcon(R.drawable.ic_folder);
     }
     return super.onOptionsItemSelected(item);
-  }
-
-  public static class FragmentAdapter extends FragmentStatePagerAdapter {
-    Context context;
-    int tabCount;
-
-    public FragmentAdapter(Context context, FragmentManager fm, int tabCount) {
-      super(fm);
-      this.context = context;
-      this.tabCount = tabCount;
-    }
-
-    @Override
-    public int getCount(){
-      return tabCount;
-    }
-
-    @Override
-    public CharSequence getPageTitle(int _position) {
-      if (_position == 0) return "Code";
-      if (_position == 1) return "Layout";
-      if (_position == 2) return "Files";
-      else return null;
-    }
-
-    @Override
-    public Fragment getItem(int _position) {
-      if (_position == 0) return new EditorCodeFragment();
-      if (_position == 1) return new EditorLayoutFragment();
-      if (_position == 2) return new EditorFilesFragment();
-      else return null;
-    }
   }
 }
